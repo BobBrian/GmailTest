97e9258db7ae0db9129979d49922c5c3b706f3e2
Bob <rellrocker235@gmail.com>
2021-02-25 17:36:53 +0200

    Set Up Part 2
    Removal of Unnecessary Data and Addition of files for Java script files and css

---
+++ gmailclone/src/Componets/Header.js
@@ -0,0 +1,13 @@
+// This will be the top bar that display the gmail Logo, The Search Bar for Emails and the Profile Logo to Log out from
+import React from 'react'
+import './CSS/Header.css'
+
+function Header() {
+    return (
+        <div classname="header">
+            This is the Header
+        </div>
+    )
+}
+
+export default Header
