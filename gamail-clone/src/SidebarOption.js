e5c94bcf0f8b29adc396e2e08da1db865246581f
Bob <rellrocker235@gmail.com>
2021-03-01 20:59:21 +0200

    Sidebar Option Part 1
    Basically The Quick construction of the Base Buttons form which different categories of Mail will be Stored

--- gamail-clone/src/SidebarOption.js
+++ gamail-clone/src/SidebarOption.js
@@ -1,9 +1,11 @@
+// The Sidebar Option will be used for Storing Categories for Mail such as the Inox, Spam and Drafted
 import React from 'react'
 
-function SidebarOption() {
+function SidebarOption({Icon, title }) {
     return (
-        <div>
-            
+        <div className="sidebarOption">
+            <Icon/>
+            <h3>{title}</h3>
         </div>
     )
 }
