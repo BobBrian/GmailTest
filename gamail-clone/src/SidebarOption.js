7ee42257a3a3c01962c7c0babcf858fee5842dcb
Bob <rellrocker235@gmail.com>
2021-03-03 23:06:59 +0200

    Sidebar Part 2
    Creation of the Standard Sidebar Elements and Css configuration to highlight selected Sidebar Options

--- gamail-clone/src/SidebarOption.js
+++ gamail-clone/src/SidebarOption.js
@@ -1,11 +1,13 @@
 // The Sidebar Option will be used for Storing Categories for Mail such as the Inox, Spam and Drafted
 import React from 'react'
+import "./SidebarOption.css"
 
-function SidebarOption({Icon, title }) {
+function SidebarOption({Icon, title, number , selected}) {
     return (
-        <div className="sidebarOption">
+        <div className={`sidebarOption ${selected && 'sidebar--active'}`}>
             <Icon/>
             <h3>{title}</h3>
+            <p>{number}</p>
         </div>
     )
 }
